e052186bf0eee1e6e424cd8ddc4cf0c855957254
Yoooga0824 <17582495726@163.com>
2026-06-01 23:40:34 +0800

    6,1儿童节快乐！

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -32,6 +32,7 @@ private:
     bool m_stop;                //是否结束线程
     connection_pool *m_connPool;  //数据库
 };
+
 template <typename T>
 threadpool<T>::threadpool( connection_pool *connPool, int thread_number, int max_requests) : m_thread_number(thread_number), m_max_requests(max_requests), m_stop(false), m_threads(NULL),m_connPool(connPool)
 {
